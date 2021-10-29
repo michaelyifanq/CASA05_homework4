687560f8ee274ee64ecd2854f82f4993d4d34661
Yifan Qian <yifan.qian.21@ucl.ac.uk>
2021-10-29 19:06:49 +0800

    commit 10_29_19:06

--- CASA05_homework4.R
+++ CASA05_homework4.R
@@ -2,13 +2,16 @@ library(sf)
 library(here)
 library(janitor)
 library(tidyverse)
+library(countrycode)#1 really great stuff
 
 countries <- st_read(here("World_Countries_(Generalized)", "World_Countries__Generalized_.shp"))%>% 
   clean_names()
-head(countries,10)
+head(countries,20)
 
-csv <- read_csv('Gender Inequality Index (GII)_trim.csv',skip=5)%>%
+csv <- read_csv(here('Gender Inequality Index (GII)_trim.csv',skip=5,na = "..",col_types=cols(year_2010=col_number(),year_2019=col_number())))%>%
   clean_names()
+
+head(csv,20)
 
 joined_data <- countries  %>%
   left_join(., 
